edb9c15fdd52bf74c05d095df5fa781296c1e210
Максим Коньков <soft.snake867@gmail.com>
2020-04-26 02:32:04 +0500

    third test

--- 0_python_basics/verification_work/test_3.py
+++ 0_python_basics/verification_work/test_3.py
@@ -10,3 +10,36 @@ while (balls >= necessary_balls):
   lines += 1
 
 print('Количество рядов:', lines)
+
+# task 2
+print('Введите числа через пробел:')
+numbers = input().split(' ')
+numbers = map(int, numbers)
+numbers = list(numbers)
+print('Введите число b:')
+b = int(input())
+numbers = list(filter(lambda x: x > b, numbers))
+ft = __import__('functools')
+result = ft.reduce(lambda acc,cv: acc + cv, numbers)
+print('Сумма элементов, больших числа b:', result)
+
+# task 3
+print('Введите x (x > 0, x < 1):')
+x = float(input())
+
+if (x > 0 and x < 1):
+  divider = 1
+  current_member = 1 / (divider ** 2)
+  row_result = current_member
+  while(current_member >= x):
+    divider += 1
+    current_member = 1 / (divider ** 2)
+    row_result += current_member
+
+  print('Сумма ряда:', row_result)
+  print('При x:', x)
+  print('Со значением последнего элемента:', current_member)
+else:
+  print('Неверно указано число.')
+  sys = __import__('sys')
+  sys.exit()
